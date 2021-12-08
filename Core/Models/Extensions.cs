496ab3732687836e57956f1fe649afb19cfc1699
Денис Лебедев <denis03lebedev@gmail.com>
2021-12-08 14:22:01 +0700

    Rewrite

---
+++ Core/Models/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QiwiBillApi.Core.Models
+{
+    public static class Extensions
+    {
+        public static string GetDescription(this Enum Element) => (Element.GetType().GetMember(Element.ToString())[0].GetCustomAttributes(typeof(DescriptionAttribute), false)[0] as DescriptionAttribute).Description;
+    }
+}
