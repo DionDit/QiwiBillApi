56a5b8a34c8d89e5e63c1fe169c61a971d15aced
Денис Лебедев <denis03lebedev@gmail.com>
2021-12-08 14:15:11 +0700

    Rewrite

---
+++ Core/Models/Enums/InvoicePaymentType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QiwiBillApi.Core.Models.Enums
+{
+    public enum InvoicePaymentType
+    {
+        /// <summary>
+        /// Счет выставлен, ожидает оплаты
+        /// </summary>
+        [Description("WAITING")]
+        WAITING,
+
+        /// <summary>
+        /// Счет оплачен
+        /// </summary>
+        [Description("PAID")]
+        PAID,
+
+        /// <summary>
+        /// Счет отклонен
+        /// </summary>
+        [Description("REJECTED")]
+        REJECTED,
+
+        /// <summary>
+        /// Время жизни счета истекло. Счет не оплачен
+        /// </summary>
+        [Description("EXPIRED")]
+        EXPIRED,
+    }
+}
