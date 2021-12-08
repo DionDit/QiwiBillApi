58300b4fb45418b536a248cd60e82b5dbc773fbf
Денис Лебедев <denis03lebedev@gmail.com>
2021-12-08 14:06:41 +0700

    Rewrite

--- Core/Models/Response/Status.cs
+++ Core/Models/Response/Status.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QiwiBillApi.Core.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -14,5 +15,28 @@ namespace QiwiBillApi.Core.Models.Response
         public string Value { get; private set; }
         [DataMember(Name = "changedDateTime")]
         public DateTime ChangedDateTime { get; private set; }
+        public InvoicePaymentType InvoicePaymentType
+        {
+            get
+            {
+                var Type = InvoicePaymentType.WAITING;
+                switch (Value)
+                {
+                    case "WAITING":
+                        Type = InvoicePaymentType.WAITING;
+                        break;
+                    case "EXPIRED":
+                        Type = InvoicePaymentType.EXPIRED;
+                        break;
+                    case "PAID":
+                        Type = InvoicePaymentType.PAID;
+                        break;
+                    case "REJECTED":
+                        Type = InvoicePaymentType.REJECTED;
+                        break;
+                }
+                return Type;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using QiwiBillApi.Core.Models;
+using QiwiBillApi.Core.Models.Enums;
 using QiwiBillApi.Core.Models.Response;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,10 @@ namespace QiwiBillApi
     {
         static void Main(string[] args)
         {
-
+            Qiwi Qiwi = new Qiwi("eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjZxcHBhdy0wMCIsInVzZXJfaWQiOiI3OTEzODE1MTYwNyIsInNlY3JldCI6ImVjYWM0ZjBiOTJiYWUwYjBkOWRiNmYxOGViOWIyZDM0YmNiMjA0YzI4YWQxYmM4ZWU3Y2ZjOTE5OTQzNjI4MjMifX0=");
+            var RejectedOrder = Qiwi.OrderGetInfo("11aa6981-3ae4-48ec-ba20-ef798e5282f9");
+            Console.WriteLine($"SiteId: {RejectedOrder.SiteId}\nBillId: {RejectedOrder.BillId}\nAmount: Value:{RejectedOrder.Amount.Value} Currency: {RejectedOrder.Amount.Currency}\nStatus: Value: {RejectedOrder.Status.Value} ChangedDateTime: {RejectedOrder.Status.ChangedDateTime}\nCustomer: Email: {RejectedOrder.Customer.Email} Phone: {RejectedOrder.Customer.Phone} Account: {RejectedOrder.Customer.Account}\nComent: {RejectedOrder.Comment}\nCreationDateTime: {RejectedOrder.CreationDateTime}\nExpirationDateTime: {RejectedOrder.ExpirationDateTime}\nUrl: {RejectedOrder.Url}");
+            Console.WriteLine(RejectedOrder.Amount.CurrencyType);
         }
     }
 }
