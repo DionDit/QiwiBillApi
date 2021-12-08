a7fd75bdd39db5d4a1c3e973a05215adc719732e
Денис Лебедев <denis03lebedev@gmail.com>
2021-12-08 14:46:29 +0700

    Revert "Revert "Rewrite""
    This reverts commit a7c00e20f86b527102898b7705e809b97e93491e.

--- Core/Models/Qiwi.cs
+++ Core/Models/Qiwi.cs
@@ -10,12 +10,18 @@ namespace QiwiBillApi.Core.Models
 {
     public class Qiwi
     {
+        private WebClient WebClient { get; set; }
         public string SecretKey { get; }
         public Qiwi(string SecretKey)
         {
             if (!string.IsNullOrWhiteSpace(SecretKey))
             {
                 this.SecretKey = SecretKey;
+                WebClient = new WebClient();
+                WebClient.Encoding = Encoding.UTF8;
+                WebClient.Headers[HttpRequestHeader.Authorization] = $"Bearer {SecretKey}";
+                WebClient.Headers[HttpRequestHeader.Accept] = "application/json";
+                WebClient.Headers[HttpRequestHeader.ContentType] = "application/json";
             }
             else
             {
@@ -23,44 +29,17 @@ namespace QiwiBillApi.Core.Models
             }
         }
 
-        private void SetHeaders(WebClient WebClient)
-        {
-            WebClient.Encoding = Encoding.UTF8;
-            WebClient.Headers[HttpRequestHeader.Authorization] = $"Bearer {SecretKey}";
-            WebClient.Headers[HttpRequestHeader.Accept] = "application/json";
-            WebClient.Headers[HttpRequestHeader.ContentType] = "application/json";
-        }
-
-        public OrderInfo OrderGetInfo(string BillId)
-        {
-            using (WebClient WebClient = new WebClient())
-            {
-                SetHeaders(WebClient);
-                return OrderInfo.CreateOrderInfoFromJSON(WebClient.DownloadString($"https://api.qiwi.com/partner/bill/v1/bills/{BillId}"));
-            }
-
-        }
+        public OrderInfo OrderGetInfo(string BillId) => OrderInfo.CreateOrderInfoFromJSON(WebClient.DownloadString($"https://api.qiwi.com/partner/bill/v1/bills/{BillId}"));
         public async Task<OrderInfo> OrderGetInfoAsync(string BillId) => await Task.Run(() => OrderGetInfo(BillId));
 
         public OrderInfo CreateOrder(Order Order)
         {
-            using (WebClient WebClient = new WebClient())
-            {
-                SetHeaders(WebClient);
-                WebClient.UploadString($"https://api.qiwi.com/partner/bill/v1/bills/{Order.BillId}", WebRequestMethods.Http.Put, Order.CreateOrderStringJSON(Order));
-                return OrderInfo.CreateOrderInfoFromJSON(WebClient.DownloadString($"https://api.qiwi.com/partner/bill/v1/bills/{Order.BillId}"));
-            }
+            WebClient.UploadString($"https://api.qiwi.com/partner/bill/v1/bills/{Order.BillId}", WebRequestMethods.Http.Put, Order.CreateOrderStringJSON(Order));
+            return OrderInfo.CreateOrderInfoFromJSON(WebClient.DownloadString($"https://api.qiwi.com/partner/bill/v1/bills/{Order.BillId}"));
         }
         public async Task<OrderInfo> CreateOrderAsync(Order Order) => await Task.Run(() => CreateOrder(Order));
 
-        public OrderInfo RejectOrder(string BillId)
-        {
-            using (WebClient WebClient = new WebClient())
-            {
-                SetHeaders(WebClient);
-                return OrderInfo.CreateOrderInfoFromJSON(WebClient.UploadString($"https://api.qiwi.com/partner/bill/v1/bills/{BillId}/reject", WebRequestMethods.Http.Post, ""));
-            }
-        }
+        public OrderInfo RejectOrder(string BillId) => OrderInfo.CreateOrderInfoFromJSON(WebClient.UploadString($"https://api.qiwi.com/partner/bill/v1/bills/{BillId}/reject", WebRequestMethods.Http.Post, ""));
         public async Task<OrderInfo> RejectOrderAsync(string BillId) => await Task.Run(() => RejectOrder(BillId));
     }
 }
